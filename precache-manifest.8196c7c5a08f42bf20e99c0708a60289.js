ffdccebe2c0219a4b34fbe4eda9a8bcf9d3d2e79
Eric Owusu <llkoole7@gmail.com>
2019-12-09 20:18:19 -0800

    Updates

---
+++ precache-manifest.8196c7c5a08f42bf20e99c0708a60289.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177bf74d464a5b2f545274e8456cd281",
+    "url": "/Dad_App_Demo/index.html"
+  },
+  {
+    "revision": "5e53be42106693c3e1d9",
+    "url": "/Dad_App_Demo/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "86dc49a8b57cf928885e",
+    "url": "/Dad_App_Demo/static/css/main.caad7731.chunk.css"
+  },
+  {
+    "revision": "5e53be42106693c3e1d9",
+    "url": "/Dad_App_Demo/static/js/2.dc085cb7.chunk.js"
+  },
+  {
+    "revision": "9ff1df13142c1e6716bd2e1faf417c84",
+    "url": "/Dad_App_Demo/static/js/2.dc085cb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86dc49a8b57cf928885e",
+    "url": "/Dad_App_Demo/static/js/main.1259af5a.chunk.js"
+  },
+  {
+    "revision": "7ccacfd89adc3899eeec",
+    "url": "/Dad_App_Demo/static/js/runtime-main.438b4248.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/Dad_App_Demo/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/Dad_App_Demo/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/Dad_App_Demo/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Dad_App_Demo/precache-manifest.8196c7c5a08f42bf20e99c0708a60289.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dad_App_Demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
